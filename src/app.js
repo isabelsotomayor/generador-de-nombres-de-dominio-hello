190f3ca150ebe37c25867a93b2d7ab4669b03e76
isabelsotomayor <isabel.sotomayor.c@gmail.com>
2025-04-15 16:02:12 +0000

    generador de nombres

--- src/app.js
+++ src/app.js
@@ -8,4 +8,16 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  for (let i=0; i<pronoun.length; i++){ 
+    for (let a=0; a<adj.length; a++){
+      for (let b=0; b<noun.length; b++){
+        console.log (pronoun[i]+adj[a]+ noun[b]+".com")
+      }
+      
+    }
+  }
+
 };
